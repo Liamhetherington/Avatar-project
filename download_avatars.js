26daf5fca9f4fa46f5814f344036b662864fd67b
Liam Hetherington <liambhetherington@gmail.com>
2019-05-07 01:04:16 +0000

    same as before but iterates over the new parsed body (array of objects) and prints only the avatar_url key for each object in the array

--- download_avatars.js
+++ download_avatars.js
@@ -15,10 +15,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, function(err, res, body) {
     var parsedBody = JSON.parse(body);
     cb(err, parsedBody);
+    for (var i =0; i < parsedBody.length; i++) {
+      console.log(parsedBody[i].avatar_url); //iterates through parsed body(array of objects)
+                                            //and pushes it to the index
+    }
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
 });
